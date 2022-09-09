136cfc3b8d32e015121663e1bf581abc7e4fdcaa
nemanja.djurisic <nemanja.djurisic@gmail.com>
2022-09-09 15:42:45 +0200

    cocktails

--- src/test/java/com/postman/constants/AuthPostman.java
+++ src/test/java/com/postman/constants/AuthPostman.java
@@ -1,6 +1,6 @@
 package com.postman.constants;
 
 public class AuthPostman {
-    public static final String API_KEY = "PMAK-63035d0a63863d6f23a90f64-ee47e6e2af6dda75ec0f85b20b91d6eca6";
+    public static final String API_KEY = "";
 
 }
